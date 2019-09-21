6837c77b9758762ed602e395fe7d8b54f515c597
Steve <stevebezalel215@gmail.com>
2019-09-21 22:23:37 +0700

    benerin csv dan tambahin action

--- player.py
+++ player.py
@@ -39,13 +39,13 @@ class Player:
 			"Minum Jus": (lambda x: x.sign(-5,10,0)),
 			"Buang Air Kecil": (lambda x: x.sign(5,0,0)),
 			"Buang Air Besar": (lambda x: x.sign(10,-5,0)),
-			# Ubah yg di bawah
-			"Bersosialisasi ke Kafe": (lambda x: x.sign(0,0,0)),
-			"Bermain Media Sosial": (lambda x: x.sign(0,0,0)),
-			"Bermain Komputer": (lambda x: x.sign(0,0,0)),
-			"Mandi": (lambda x: x.sign(0,0,0)),
-			"Cuci Tangan": (lambda x: x.sign(0,0,0)),
-			"Membaca Koran": (lambda x: x.sign(0,0,0)),
-			"Membaca Novel": (lambda x: x.sign(0,0,0))
+			"Bersosialisasi ke Kafe": (lambda x: x.sign(-5,-10,15)),
+			"Bermain Media Sosial": (lambda x: x.sign(0,-10,10)),
+			"Bermain Komputer": (lambda x: x.sign(0,-10,15)),
+			"Mandi": (lambda x: x.sign(15,-5,0)),
+			"Cuci Tangan": (lambda x: x.sign(5,0,0)),
+			"Mendengarkan Musik di Radio": (lambda x: x.sign(0,-5,10)),
+			"Membaca Koran": (lambda x: x.sign(0,-5,5)),
+			"Membaca Novel": (lambda x: x.sign(0,-5,10))
 		}
 		choices.get(aksi, (lambda x: print("Opsi tidak ada")))(self)
